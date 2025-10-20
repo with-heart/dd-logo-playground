df6777a30ed4d823b06d1a48fb51184c74578da5
with-heart <with.heart+git@pm.me>
2025-10-20 14:31:51 -0400

    add github repo link to app header

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,19 @@
 'use client'
+import { Button } from '@/components/ui/button'
 import { Logo } from '@/logo/client'
 import { Sidebar } from '../sidebar'
+
+const GithubSvg = () => {
+  return (
+    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <title>GitHub</title>
+      <path
+        className="fill-white"
+        d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+      />
+    </svg>
+  )
+}
 
 export function App() {
   return (
@@ -8,8 +21,18 @@ export function App() {
       id="root"
       className="grid h-screen grid-cols-[4fr_minmax(300px,_1fr)] grid-rows-[auto_1fr_auto] gap-1 bg-zinc-700 p-2 *:bg-zinc-950"
     >
-      <header className="px-4 py-2 text-center font-medium [grid-area:_header]">
-        Developer DAO Logo Playground
+      <header className="px-4 py-2 flex items-center justify-between [grid-area:_header]">
+        <h1 className="font-medium">Developer DAO Logo Playground</h1>
+        <Button size="icon-lg" variant="outline" asChild>
+          <a
+            href="https://github.com/with-heart/dd-logo-playground"
+            title="GitHub Repository"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GithubSvg />
+          </a>
+        </Button>
       </header>
       <article className="@container flex items-center justify-center p-4 [grid-area:_image] *:aspect-square *:w-[75cqmin]">
         <Logo />
